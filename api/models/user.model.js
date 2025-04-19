a07772b6dbe6ef6b16cb12d6e46bd8cd43d741b5
GIZDEDOR <igorek-rybalchenko@mail.ru>
2025-04-19 04:29:32 +0300

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    avatar: {
+        type: String,
+        default: "https://www.manageengine.com/images/speaker-placeholder.png"
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [ formData, setFormData ] = useState({});
@@ -42,13 +43,14 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Регистрация</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='Логин'
+        <input type="text" placeholder='Имя'
         className='border p-3 rounded-lg' id='username'onChange={handleChange} />
         <input type="email" placeholder='Почта'
         className='border p-3 rounded-lg' id='email'onChange={handleChange} />
         <input type="password" placeholder='Пароль'
         className='border p-3 rounded-lg' id='password'onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '>{loading ? 'Загрузка...' : 'Зарегистрироваться'}</button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>У вас уже есть учётная запись?</p>
