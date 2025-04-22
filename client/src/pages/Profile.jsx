ec716ca68496d9d3de3bf1fff462db854fd6b48d
GIZDEDOR <igorek-rybalchenko@mail.ru>
2025-04-22 21:45:33 +0300

    complete create listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { updateUserStart, updateUserFailure, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserSuccess, signOutUserFailure, signOutUserStart } from "../redux/user/userSlice";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom';
 
 
 export default function Profile() {
@@ -162,6 +163,9 @@ export default function Profile() {
           onChange={handleChange} 
         />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Загрузка...': 'Обновить'}</button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Выложить объект
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Удалить учётную запись</span>
