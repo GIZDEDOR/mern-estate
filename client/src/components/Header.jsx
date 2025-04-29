c64ab57f409e1656da6b314bff78e90c4efed890
GIZDEDOR <igorek-rybalchenko@mail.ru>
2025-04-30 02:55:26 +0300

    complete header serach form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import {FaSearch} from 'react-icons/fa';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Headre() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser} = useSelector(state => state.user);
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -13,10 +33,17 @@ export default function Headre() {
           <span className='text-slate-700'>nedvizh</span>
         </h1>
         </Link>
-        <form className='bg-white ring-1 ring-slate-900/5 rounded-full p-3 flexBetween gap-x-2 relative flex items-center'>
-          <input type="text" placeholder='Поиск...' 
-          className='outline-none border-none w-24 sm:w-64 bg-white '/>
-          <FaSearch className='text-slate-600' />
+        <form onSubmit={handleSubmit} className='bg-white ring-1 ring-slate-900/5 rounded-full p-3 flexBetween gap-x-2 relative flex items-center'>
+          <input 
+            type="text" 
+            placeholder='Поиск...' 
+            className='outline-none border-none w-24 sm:w-64 bg-white '
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
         <ul className='flex gap-4'>
             <Link to='/'>
